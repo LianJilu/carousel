2ed1dad706a0a078681464e6eb1f513b68e01ab8
Jimmy <408224334@qq.com>
2018-09-28 21:06:33 +0800

    'modify'

--- Carousel-ES6.js
+++ Carousel-ES6.js
@@ -1,121 +1,117 @@
 
-class Carousel {
-    constructor (el, options = {}) {
-        this.options = options
-        // this.options.onMoved = function(e){};
-        this.el = el
-        this.location = 0
-        this.cardNum = el.querySelectorAll(".carousel-card").length
-        this.btnNext = el.querySelector(".carousel-control_next")
-        this.btnPrev = el.querySelector(".carousel-control_prev")
-        const cardNum = this.cardNum
-        const visualCardNum = this.getVisualCardNum()
-        this.scrollableNum = 0
+    export default class Carousel {
+        constructor (el, options = {}) {
+            this.options = options
+            this.el = el
+            this.location = 0
+            this.scrollableNum = 0
+            this.cardNum = el.querySelectorAll(".carousel-card").length
+            this.btnNext = el.querySelector(".carousel-control_next")
+            this.btnPrev = el.querySelector(".carousel-control_prev")
 
-        this.btnPrev.classList.add("disable")
-        if (visualCardNum >= cardNum){
-            this.btnNext.classList.add("disable")
-            return
-        }
-        this.scrollableNum = cardNum - visualCardNum
-        this.btnNext.addEventListener("click", this.next.bind(this));
-        this.btnPrev.addEventListener("click", this.prev.bind(this));
-    }
+            const cardNum = this.cardNum
+            const visualCardNum = this.getVisualCardNum()
 
-    getCardLeng() {
-        const card = this.el.querySelector(".carousel-card");
-        const computedStyle = window.getComputedStyle(card, null);
-
-        if (this.options.vertical){
-            return card.offsetHeight + parseInt(computedStyle.marginTop) + parseInt(computedStyle.marginBottom);
-        }else{
-            return card.offsetWidth + parseInt(computedStyle.marginLeft) + parseInt(computedStyle.marginRight);
-        }
-    }
-
-    getVisualLeng () {
-        const viewPort = this.el.querySelector(".carousel-viewport");
-
-        if (this.options.vertical){
-            return viewPort.clientHeight;
-        }else{
-            return viewPort.clientWidth;
-        }
-    }
-
-    getVisualCardNum() {
-        const cardLength = this.getCardLeng();
-        const visualLength = this.getVisualLeng();
-        const visualCardNum = Math.ceil(visualLength / cardLength);
-        return visualCardNum;
-    }
-
-    move() {
-        const el = this.el;
-        const shelf = this.el.querySelector(".carousel-shelf");
-        const cardLength = this.getCardLeng();
-        const duration = this.options.duration || 400;
-        const absoluteLength = cardLength * this.location;
-        
-        const e = new Event("moved");
-
-        shelf.style.transitionDuration = duration + "ms";
-        if (this.options.vertical){
-            shelf.style.top = -absoluteLength + "px";
-        } else {
-            shelf.style.left = -absoluteLength + "px";
+            this.btnPrev.classList.add("disable")
+            if (visualCardNum >= cardNum){
+                this.btnNext.classList.add("disable")
+                return
+            }
+            this.scrollableNum = cardNum - visualCardNum
+            this.btnNext.addEventListener("click", this.next.bind(this))
+            this.btnPrev.addEventListener("click", this.prev.bind(this))
         }
 
-        // el.dispatchEvent(e);
-        // debugger;
-        this.options.onMoved(e);
-    }
+        getCardLeng() {
+            const card = this.el.querySelector(".carousel-card")
+            const computedStyle = window.getComputedStyle(card, null)
 
-    next() {
-        const location = this.location;
-        const cardNum = this.cardNum;
-        const visualCardNum = this.getVisualCardNum();
-        const scrollingNum = cardNum - visualCardNum - location > 0 ? cardNum - visualCardNum - location : 0;
-        const btnNext = this.btnNext;
-        const btnPrev = this.btnPrev;
-        let relateLocation = 1;
-        
-        if (btnNext.classList.contains("disable")) return;
-
-        if (this.options.multi){
-            if (scrollingNum > visualCardNum) {
-                relateLocation = visualCardNum;
-            } else {
-                relateLocation = scrollingNum;
+            if (this.options.vertical){
+                return card.offsetHeight + parseInt(computedStyle.marginTop) + parseInt(computedStyle.marginBottom)
+            }else{
+                return card.offsetWidth + parseInt(computedStyle.marginLeft) + parseInt(computedStyle.marginRight)
             }
         }
 
-        this.location = location + relateLocation;
-        this.move();
-        if (this.location === this.scrollableNum) btnNext.classList.add("disable");
-        btnPrev.classList.remove("disable");
-    }
+        getVisualLeng () {
+            const viewPort = this.el.querySelector(".carousel-viewport")
 
-    prev() {
-        const location = this.location;
-        const btnNext = this.btnNext;
-        const btnPrev = this.btnPrev;
-        const visualCardNum = this.getVisualCardNum();
-        let relateLocation = 1;
-
-        if (btnPrev.classList.contains("disable")) return;
-        
-        if (this.options.multi) {
-            if (location > visualCardNum) {
-                relateLocation = visualCardNum;
-            } else {
-                relateLocation = location;
+            if (this.options.vertical){
+                return viewPort.clientHeight
+            }else{
+                return viewPort.clientWidth
             }
         }
-        
-        this.location = location - relateLocation;
-        this.move();
-        if (this.location === 0) btnPrev.classList.add("disable");
-        btnNext.classList.remove("disable");
+
+        getVisualCardNum() {
+            const cardLength = this.getCardLeng()
+            const visualLength = this.getVisualLeng()
+            const visualCardNum = Math.ceil(visualLength / cardLength)
+            return visualCardNum
+        }
+
+        move() {
+            const shelf = this.el.querySelector(".carousel-shelf")
+            const cardLength = this.getCardLeng()
+            const duration = this.options.duration || 400
+            const absoluteLength = cardLength * this.location
+
+            shelf.style.transitionDuration = duration + "ms"
+            if (this.options.vertical){
+                shelf.style.top = -absoluteLength + "px"
+            } else {
+                shelf.style.left = -absoluteLength + "px"
+            }
+
+            if (this.options.onMoved) this.options.onMoved()  //trigger ovMoved Event
+        }
+
+        next() {
+            const location = this.location
+            const cardNum = this.cardNum
+            const visualCardNum = this.getVisualCardNum()
+            const scrollingNum = cardNum - visualCardNum - location > 0 ? cardNum - visualCardNum - location : 0
+            const btnNext = this.btnNext
+            const btnPrev = this.btnPrev
+            let relateLocation = 1
+            
+            if (btnNext.classList.contains("disable")) return
+
+            if (this.options.multi){
+                if (scrollingNum > visualCardNum) {
+                    relateLocation = visualCardNum
+                } else {
+                    relateLocation = scrollingNum
+                }
+            }
+
+            this.location = location + relateLocation
+            this.move()
+            if (this.location === this.scrollableNum) btnNext.classList.add("disable")
+            btnPrev.classList.remove("disable")
+        }
+
+        prev() {
+            const location = this.location
+            const btnNext = this.btnNext
+            const btnPrev = this.btnPrev
+            const visualCardNum = this.getVisualCardNum()
+            let relateLocation = 1
+
+            if (btnPrev.classList.contains("disable")) return
+            
+            if (this.options.multi) {
+                if (location > visualCardNum) {
+                    relateLocation = visualCardNum
+                } else {
+                    relateLocation = location
+                }
+            }
+            
+            this.location = location - relateLocation
+            this.move()
+            if (this.location === 0) btnPrev.classList.add("disable")
+            btnNext.classList.remove("disable")
+        }
     }
-}
+
