f4d340db42c0408190c636a5f7045646b5499773
Jimmy <408224334@qq.com>
2018-10-09 15:28:01 +0800

    ES module in modern browser

---
+++ src/jQuery-Plugin-ES6.js
@@ -0,0 +1,25 @@
+
+import Carousel from './Carousel-ES6.js';
+
++function($){
+    function Plugin(option){
+        return this.each(function(){
+            var $this = $(this);
+            var data = $this.data("carousel");
+
+            if (!data) $this.data("carousel",(data = new Carousel(this, option)));
+        })
+    }
+
+    var old = $.fn.carousel;
+
+    $.fn.carousel = Plugin;
+    $.fn.carousel.Constructor = Carousel;
+
+    $.fn.carousel.noConflict = function(){
+        $.fn.carousel = old;
+        return this;
+    }
+}(jQuery)
+    
+
